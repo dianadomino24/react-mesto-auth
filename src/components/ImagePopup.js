3c64077cf1eefc2c7a56ad6829b8a07268f4adcc
Diana Vlasenkova <dianadomino24work@gmail.com>
2020-11-22 13:53:21 +0300

    1.4 imgPopup fix + prett

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -18,32 +18,30 @@ function ImagePopup({ card, onClose, isOpen, name }) {
         }
     }
     if (isOpen) {
-        window.addEventListener('keydown', handleEscClose)}
+        window.addEventListener('keydown', handleEscClose)
+    }
 
     return (
-            <section
-                className={`popup popup_type_${name} ${isOpen && 'popup_opened'}
+        <section
+            className={`popup popup_type_${name} ${isOpen && 'popup_opened'}
                 `}
-                onClick={closePopupByClickingOverlay}
-            >
-                <div className="popup__container-pic-zoom">
-                    <button
-                        className="link popup__close-button"
-                        onClick={close}
+            onClick={closePopupByClickingOverlay}
+        >
+            <div className="popup__container-pic-zoom">
+                <button className="link popup__close-button" onClick={close} />
+                <figure className="picture-zoom">
+                    <img
+                        src={card ? card.link : ''}
+                        alt={card ? card.name : ''}
+                        className="picture-zoom__img"
                     />
-                    <figure className="picture-zoom">
-                        <img
-                            src={card? card.link : ''}
-                            alt={card? card.name : ''}
-                            className="picture-zoom__img"
-                        />
-                        <p className="picture-zoom__title">{card? card.name : ''}</p>
-                    </figure>
-                </div>
-            </section>
+                    <p className="picture-zoom__title">
+                        {card ? card.name : ''}
+                    </p>
+                </figure>
+            </div>
+        </section>
     )
-
 }
-
 
 export default ImagePopup
